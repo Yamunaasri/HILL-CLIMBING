b5baed2acc782e9cbabeb6bfdff6a535674da358
Yamunaasri <115707860+Yamunaasri@users.noreply.github.com>
2024-09-26 09:01:22 +0530

    Update hill climbing.py

--- hill climbing.py
+++ hill climbing.py
@@ -7,7 +7,7 @@
 import random
 import string
 def generate_random_solution(answer):
-    l=TYPE THE CODE HERE ,FIND THE LEN OF answer and store in l
+    l=len(answer)
     return [random.choice(string.printable) for _ in range(l)]
 def evaluate(solution,answer):
     print(solution)
@@ -20,8 +20,7 @@ def evaluate(solution,answer):
         #ord(s) - ord(t) calculates the difference between the ASCII values of the characters s and t.
          #abs() takes the absolute value of this difference to ensure that it is non-negative. This is important because the difference could be negative if s is less than t in terms of ASCII value.
          #The absolute value ensures that the difference is always positive or zero.
-        diff +=Type the code here by referring to the above description 
-    return diff
+        diff += abs(ord(s) - ord(t))    return diff
 def mutate_solution(solution):
     ind=random.randint(0,len(solution)-1)
     solution[ind]=random.choice(string.printable)
@@ -31,17 +30,13 @@ def SimpleHillClimbing():
     best=generate_random_solution(answer)
     best_score=evaluate(best,answer)
     while True:
-        print("Score:",best_score," Solution : ","".join(best))  
-        if best_score==0:
-            break
-        new_solution=mutate_solution(list(best))
-        score=evaluate(new_solution,answer)   
-        if score<best_score:
-            best=new_solution
-            best_score=score
-#answer="Artificial Intelligence"
-#print(generate_random_solution(answer))
-#solution=generate_random_solution(answer)
-#print(evaluate(solution,answer))
+       print("Score:", best_score, " Solution: ", "".join(best))  
+       if best_score == 0:
+           break
+       new_solution = mutate_solution(list(best))
+       score = evaluate(new_solution, answer)
+       if score < best_score:
+           best = new_solution
+           best_score = score
 SimpleHillClimbing()
 
